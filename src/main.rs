186f13e07145fbac8c29b986632e504953be5ec6
valkyrie_pilot <valk@randomairborne.dev>
2023-11-17 21:09:18 -0700

    Initial parsing commit

---
+++ src/main.rs
@@ -0,0 +1,184 @@
+use chrono::Utc;
+use serde::Deserialize;
+use std::fs::OpenOptions;
+use std::path::PathBuf;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
+use std::time::Instant;
+
+use serde_with::{serde_as, DisplayFromStr};
+
+#[derive(serde::Serialize, Clone)]
+struct Reply {
+    prompt: String,
+    reply: String,
+}
+
+#[derive(serde::Serialize, Clone)]
+struct Message {
+    content: String,
+    timestamp: chrono::DateTime<Utc>,
+    author: u64,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    reference: Option<u64>,
+}
+
+#[derive(serde::Deserialize)]
+struct DiscordMessage {
+    content: String,
+    timestamp: chrono::DateTime<Utc>,
+    author: DiscordAuthor,
+    message_reference: Option<DiscordMessageReference>,
+}
+
+#[serde_as]
+#[derive(Debug, Deserialize)]
+struct DiscordAuthor {
+    #[serde_as(as = "DisplayFromStr")]
+    id: u64,
+}
+
+#[serde_as]
+#[derive(Debug, Deserialize)]
+struct DiscordMessageReference {
+    #[serde_as(as = "Option<DisplayFromStr>")]
+    message_id: Option<u64>,
+}
+
+fn main() {
+    let root_path = PathBuf::from(
+        std::env::args()
+            .nth(1)
+            .expect("Expected first argument to be path"),
+    );
+    let who_string = std::env::args()
+        .nth(2)
+        .expect("Expected a second argument of a discord user id");
+    let who: u64 = who_string.parse().unwrap();
+
+    let out_file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open(format!("./prompt-{who}.json"))
+        .unwrap();
+
+    let mut channels: Vec<Vec<Message>> = Vec::with_capacity(256);
+
+    let channel_files = channel_files(root_path);
+    let parse_start = Instant::now();
+    let total_files = channel_files.len();
+    let completed: Arc<AtomicUsize> = Arc::new(AtomicUsize::new(0));
+
+    for messages_json in channel_files {
+        let our_number = completed.fetch_add(1, Ordering::Relaxed) + 1;
+        let start = Instant::now();
+        println!("Starting parsing on {messages_json:?} ({our_number}/{total_files})");
+        let file = OpenOptions::new().read(true).open(&messages_json).unwrap();
+        let data: Vec<DiscordMessage> = simd_json::from_reader(file).unwrap();
+        let mut messages: Vec<Message> = data
+            .into_iter()
+            .map(|v| Message {
+                author: v.author.id,
+                content: v.content,
+                timestamp: v.timestamp,
+                reference: v.message_reference.map(|v| v.message_id).flatten(),
+            })
+            .collect();
+        messages.sort_by(|a, b| a.timestamp.cmp(&b.timestamp));
+        channels.push(messages);
+        let end = Instant::now();
+        let duration = end - start;
+        println!(
+            "Completed parsing on {messages_json:?} ({our_number}/{total_files}), took {}ms",
+            duration.as_millis()
+        );
+    }
+    println!(
+        "Completed all parsing in {} seconds, have {} messages from {} channels",
+        (Instant::now() - parse_start).as_secs(),
+        channels.iter().map(|v| v.len()).sum::<usize>(),
+        channels.len()
+    );
+    let mut replies: Vec<Reply> = Vec::with_capacity(100_000);
+    for channel in channels {
+        for (index, message) in channel.iter().enumerate() {
+            if message.author != who {
+                continue;
+            }
+            let reply = message.content.clone();
+            let Some(prompt) = get_prompt(&channel, index, who) else {
+                continue;
+            };
+            replies.push(Reply { prompt, reply });
+        }
+    }
+    serde_json::to_writer(out_file, &replies).unwrap();
+    println!("Done, see ya!");
+}
+
+fn get_prompt(messages: &[Message], index: usize, who: u64) -> Option<String> {
+    if index == 0 {
+        return None;
+    }
+    let mut innerdex = index - 1;
+    let reply = &messages[index];
+    let mut outputs: Vec<String> = Vec::new();
+    while innerdex != 0
+        && index - innerdex <= 5
+        && messages[innerdex].author != who
+        && (messages[innerdex].timestamp - reply.timestamp).num_minutes() < 10
+    {
+        let prompt = &messages[innerdex];
+        outputs.push(prompt.content.clone());
+        innerdex -= 1;
+    }
+    if outputs.is_empty() {
+        None
+    } else {
+        outputs.reverse();
+        Some(outputs.join("\n"))
+    }
+}
+
+fn walkdir(path: PathBuf) -> Vec<PathBuf> {
+    path.read_dir()
+        .unwrap()
+        .map(|v| v.unwrap().path())
+        .collect()
+}
+
+fn channel_files(root_path: PathBuf) -> Vec<PathBuf> {
+    let channel_dirs = walkdir(root_path);
+    let mut thread_dirs = Vec::with_capacity(1024);
+    let mut files = Vec::with_capacity(1024);
+
+    for dir in &channel_dirs {
+        let threads_dir = dir.join("threads");
+        if threads_dir.exists() {
+            let mut dirs = walkdir(threads_dir);
+            thread_dirs.append(&mut dirs);
+        } else {
+            eprintln!("Found no threads in {dir:?}, skipping..");
+        }
+    }
+
+    for dir in &channel_dirs {
+        let messages_path = dir.join("channel_messages.json");
+        if messages_path.exists() {
+            files.push(messages_path);
+        } else {
+            eprintln!("Found no channel_messages.json in {dir:?}, skipping..");
+        }
+    }
+
+    for dir in thread_dirs {
+        let messages_path = dir.join("thread_messages.json");
+        if messages_path.exists() {
+            files.push(messages_path);
+        } else {
+            eprintln!("Found no thread_messages.json in {dir:?}, skipping..");
+        }
+    }
+    files
+}
